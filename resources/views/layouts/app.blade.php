be6674c8f4a679efafb7bf9ecccaad603dae5711
rahmaamanila <rahmaamanila@gmail.com>
2023-03-01 20:36:29 +0700

    landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,86 +7,80 @@
     <title>Pengaduan Masyarakat</title>
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
 
-    <!-- Primary Meta Tags -->
-    <meta name="title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
-    <meta name="description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://uideck.com/play/">
-    <meta property="og:title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
-    <meta property="og:description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
-    <meta property="og:image" content="https://uideck.com/wp-content/uploads/2021/09/play-meta-bs.jpg">
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://uideck.com/play/">
-    <meta property="twitter:title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
-    <meta property="twitter:description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
-    <meta property="twitter:image" content="https://uideck.com/wp-content/uploads/2021/09/play-meta-bs.jpg">
+    <meta charset="utf-8">
+    
+    <!--====== Title ======-->
+    <title>Basic - SaaS Landing Page</title>
+    
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--====== Favicon Icon ======-->
-    <link
-      rel="shortcut icon"
-      href="assets/images/favicon.svg"
-      type="image/svg"
-    />
-
-    <!-- ===== All CSS files ===== -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/lineicons.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/ud-styles.css')}}" />
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" type="image/png">
+        
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+        
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+        
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+        
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/LineIcons.css')}}">
+        
+    <!--====== Font Awesome CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+        
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/default.css')}}">
+    
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 <body>
     @yield('content')
 </body>
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <!-- ====== All Javascript Files ====== -->
-    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    
+    <!--====== Jquery js ======-->
+    <script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/modernizr-3.7.1.min.js')}}"></script>
+    
+    <!--====== Bootstrap js ======-->
+    <script src="{{asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    
+    <!--====== Plugins js ======-->
+    <script src="{{asset('assets/js/plugins.js')}}"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="{{asset('assets/js/slick.min.js')}}"></script>
+    
+    <!--====== Ajax Contact js ======-->
+    <script src="{{asset('assets/js/ajax-contact.js')}}"></script>
+    
+    <!--====== Counter Up js ======-->
+    <script src="{{asset('assets/js/waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+    
+    <!--====== Scrolling Nav js ======-->
+    <script src="{{asset('assets/js/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('assets/js/scrolling-nav.js')}}"></script>
+    
+    <!--====== wow js ======-->
     <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    
+    <!--====== Particles js ======-->
+    <script src="{{asset('assets/js/particles.min.js')}}"></script>
+    
+    <!--====== Main js ======-->
     <script src="{{asset('assets/js/main.js')}}"></script>
-    <script>
-      // ==== for menu scroll
-      const pageLink = document.querySelectorAll(".ud-menu-scroll");
-
-      pageLink.forEach((elem) => {
-        elem.addEventListener("click", (e) => {
-          e.preventDefault();
-          document.querySelector(elem.getAttribute("href")).scrollIntoView({
-            behavior: "smooth",
-            offsetTop: 1 - 60,
-          });
-        });
-      });
-
-      // section menu active
-      function onScroll(event) {
-        const sections = document.querySelectorAll(".ud-menu-scroll");
-        const scrollPos =
-          window.pageYOffset ||
-          document.documentElement.scrollTop ||
-          document.body.scrollTop;
-
-        for (let i = 0; i < sections.length; i++) {
-          const currLink = sections[i];
-          const val = currLink.getAttribute("href");
-          const refElement = document.querySelector(val);
-          const scrollTopMinus = scrollPos + 73;
-          if (
-            refElement.offsetTop <= scrollTopMinus &&
-            refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
-          ) {
-            document
-              .querySelector(".ud-menu-scroll")
-              .classList.remove("active");
-            currLink.classList.add("active");
-          } else {
-            currLink.classList.remove("active");
-          }
-        }
-      }
-
-      window.document.addEventListener("scroll", onScroll);
-    </script>
 </html>
